e36a117755bacfd6d669307c3d46e9433696e93f
hugo lemos <hugolemosdasilva.hu@gmail.com>
2023-09-12 09:13:04 -0300

    push

--- stack.sh
+++ stack.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 URL=$API_URL
+URL_PEGANDO_ID="http://89.116.214.202:9000/api/endpoints/2/docker/containers/json"
+URL_FOR_PAUSA="http://89.116.214.202:9000/api/endpoints/2/docker/containers"
+URL_FOR_DELETE="http://89.116.214.202:9000/api/endpoints/2/docker/containers"
 API_KEY=$API_KEY
 STACK_NAME=$STACK_NAME
 FILE_PATH=$FILE_PATH
 imagem=hugollemos/demo:latest
-# Faz a solicitação GET e armazena a resposta em uma variável
+ENDPOINT=2
+
+ #Faz a solicitação GET e armazena a resposta em uma variável
 response=$(curl -s -X GET "$URL" -H "X-API-Key: $API_KEY" --insecure)
+
+# Obtenha o ID do contêiner com base no nome
+CONTAINER_ID=$(curl -X GET "$URL_PEGANDO_ID" -H "X-Api-Key: $API_KEY" | jq -r '.[] | select(.Names[] | contains("'$CONTAINER_NAME'")) | .Id')
 
 # Verifica se a stack com o nome $STACK_NAME está criada
 if echo "$response" | jq -e '.[] | select(.Name == "'"$STACK_NAME"'")' > /dev/null; then
@@ -14,62 +22,102 @@ if echo "$response" | jq -e '.[] | select(.Name == "'"$STACK_NAME"'")' > /dev/nu
   name=$(echo "$response" | jq -r '.[] | select(.Name == "'"$STACK_NAME"'") | .Name')
 
   # Imprime o nome da stack
-  echo "A Stack chamada $name está criada. Noome: $name"
+  echo "A Stack chamada $name está criada. Nome: $name"
 
   # Obtém o ID da stack
   id=$(echo "$response" | jq -r '.[] | select(.Name == "'"$STACK_NAME"'") | .Id')
-  
-  # Imprime o ID da stack
-  echo "Obtendo ID da stack: $id"
-  
+
   # Monta a URL para a exclusão
   DELETE_URL="$URL/$id"
-  
-  # Faz a solicitação DELETE para atualizar a stack
-  curl -X DELETE "$DELETE_URL" \
-  -H "X-API-Key: $API_KEY" \
-  -F "type=2" \
-  -F "method=file" \
-  -F "file=@$FILE_PATH" \
-  -F "endpointId=2" \
-  -F "Name=$STACK_NAME" --insecure
-  echo "Stack deletada. ID: $id"
-  sleep 5
 
-  echo "realizando o pull"
-  docker pull $imagem
-  sleep 5
-  
-  echo "CRIANDO A STACK $name"
-  response=$(curl -s -X POST "$URL" \
-  -H "X-API-Key: $API_KEY" \
-  -F "type=2" \
-  -F "method=file" \
-  -F "file=@$FILE_PATH" \
-  -F "endpointId=2" \
-  -F "Name=$STACK_NAME" --insecure)
+  if [ ! -z "$CONTAINER_ID" ]; then
+    echo "pausando container"
+    curl -X POST "$URL_FOR_PAUSA/$CONTAINER_NAME/stop" -H "X-API-Key: $API_KEY"
+    sleep 5
+    echo "deletando container"
+    curl -X DELETE "$URL_FOR_DELETE/$CONTAINER_NAME" -H "X-API-Key: $API_KEY"
+    sleep 5
+    echo "deletando stack"
+    # Faz a solicitação DELETE para atualizar a stack
+    sleep 5
+    curl -X DELETE "$DELETE_URL" \
+    -H "X-API-Key: $API_KEY" \
+    -F "type=2" \
+    -F "method=file" \
+    -F "file=@$FILE_PATH" \
+    -F "endpointId=$ENDPOINT" \
+    -F "Name=$STACK_NAME" --insecure
+    echo "Stack deletada. ID: $id"
+    sleep 5
+    echo "realizando o pull"
+    docker pull hugollemos/demo:latest
+    sleep 5
+    echo "Aguardando 5 segundos..."
+    sleep 5
+    echo "=========================================="
+    echo "CRIANDO A STACK $name"
+    echo "=========================================="
+    response=$(curl -s -X POST "$URL" \
+    -H "X-API-Key: $API_KEY" \
+    -F "type=2" \
+    -F "method=file" \
+    -F "file=@$FILE_PATH" \
+    -F "endpointId=$ENDPOINT" \
+    -F "Name=$STACK_NAME" --insecure)
 
-  # Imprimir a resposta da requisição 
-  echo "Resposta da solicitação POST: $response"
+    # Imprimir a resposta da requisição 
+    echo "Resposta da solicitação POST: $response"
 
-  # Extrair o valor do campo "Id" da nova stack usando jq
-  id=$(echo "$response" | jq -r '.Id')
+    # Extrair o valor do campo "Id" da nova stack usando jq
+    id=$(echo "$response" | jq -r '.Id')
 
-  # Imprimir o valor do Id
-  echo "Nova Stack criada. Id: $id"
+    # Imprimir o valor do Id
+    echo "Nova Stack criada. Id: $id"
+  else
+    echo "container nao encontrado"
+    echo "deletando stack"
+    curl -X DELETE "$DELETE_URL" \
+    -H "X-API-Key: $API_KEY" \
+    -F "type=2" \
+    -F "method=file" \
+    -F "file=@$FILE_PATH" \
+    -F "endpointId=$ENDPOINT" \
+    -F "Name=$STACK_NAME" --insecure
+    echo "Stack deletada. ID: $id"
+    echo "CRIANDO A NOVA STACK"
+
+    response=$(curl -s -X POST "$URL" \
+    -H "X-API-Key: $API_KEY" \
+    -F "type=2" \
+    -F "method=file" \
+    -F "file=@$FILE_PATH" \
+    -F "endpointId=$ENDPOINT" \
+    -F "Name=$STACK_NAME" --insecure)
+
+    # Imprimir a resposta da requisição 
+    echo "Resposta da solicitação POST: $response"
+
+    # Extrair o valor do campo "Id" da nova stack usando jq
+    id=$(echo "$response" | jq -r '.Id')
+
+    # Imprimir o valor do Id
+    echo "Nova Stack criada. Id: $id"
+  fi
+
 else
-  echo fazendo o pull da imagem
-  docker pull $imagem
-
-  echo "Nenhuma Stack da aplicação encontrada."
+  echo "=========================================="
+  echo "NENHUMA STACK DA APLICAÇÃO ENCONTRADA."
   echo "CRIANDO A NOVA STACK"
-
+  echo "=========================================="
+  echo "realizando o pull"
+  docker pull hugollemos/demo:latest
+  sleep 5
   response=$(curl -s -X POST "$URL" \
   -H "X-API-Key: $API_KEY" \
   -F "type=2" \
   -F "method=file" \
   -F "file=@$FILE_PATH" \
-  -F "endpointId=2" \
+  -F "endpointId=$ENDPOINT" \
   -F "Name=$STACK_NAME" --insecure)
 
   # Imprimir a resposta da requisição 
