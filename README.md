6dab7f49a1e69c059a383bdd6f8b4409f5b0632f
Hugo Lemos <hugolemosdasilva.hu@gmail.com>
2023-09-18 08:10:29 -0300

    teste

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # deploy
+
+
+```
+-
+        name: Deploy
+        uses: Hugollemos/deploy@v1
+        env:
+          API_URL: ${{ secrets.API_URL }}
+          API_KEY: ${{ secrets.API_KEY }}
+          FILE_PATH: "./docker-compose.yml"
+          STACK_NAME: ola
+          ENDPOINT: 2
+          CONTAINER_NAME: as
+          tags: nomedocontainer com tag
+          api_docker: ${{ secrets.api_docker }} // http://1.2.4.0:90/api/endpoints/2/docker 
+```
--- stack.sh
+++ stack.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 URL=$API_URL
-URL_PEGANDO_ID="http://89.116.214.202:9000/api/endpoints/2/docker/containers/json"
-URL_FOR_PAUSA="http://89.116.214.202:9000/api/endpoints/2/docker/containers"
-URL_FOR_DELETE="http://89.116.214.202:9000/api/endpoints/2/docker/containers"
 API_KEY=$API_KEY
-STACK_NAME=ola
+STACK_NAME=$STACK_NAME
 FILE_PATH=$FILE_PATH
-imagem=hugollemos/demo:latest
-ENDPOINT=2
-CONTAINER_NAME=abc
- #Faz a solicitação GET e armazena a resposta em uma variável
+ENDPOINT=$ENDPOINT
+api_docker=$api_docker
+MANIPULA_CONTAINER=$api_docker/containers
+getsha=$api_docker/images/json
+deleteimagem=$api_docker/images
+tags=$tags
+
+# Faz a solicitação GET e armazena a resposta em uma variável
 response=$(curl -s -X GET "$URL" -H "X-API-Key: $API_KEY" --insecure)
 
-# Obtenha o ID do contêiner com base no nome
-CONTAINER_ID=$(curl -X GET "$URL_PEGANDO_ID" -H "X-Api-Key: $API_KEY" | jq -r '.[] | select(.Names[] | contains("'$CONTAINER_NAME'")) | .Id')
 
-# Verifica se a stack com o nome $STACK_NAME está criada
+# Obtenha o ID do contêiner com base no nome
+CONTAINER_ID=$(curl -X GET "$MANIPULA_CONTAINER/json" -H "X-Api-Key: $API_KEY" | jq -r '.[] | select(.Names[] | contains("'$CONTAINER_NAME'")) | .Id')
+
+
+# Obtenha o SHA da imagem com base na tag
+IMAGE_SHA=$(curl -s -X GET "$getsha" -H "X-API-Key: $API_KEY" | jq -r --arg tags "$tags" '.[] | select(.RepoTags[] | contains($tags)) | .Id')
+
+# Exibe o SHA da imagem
+echo $IMAGE_SHA
+
+# Verifica se a stack está criada
 if echo "$response" | jq -e '.[] | select(.Name == "'"$STACK_NAME"'")' > /dev/null; then
 
   # Extrai o valor do campo "Name" usando jq
@@ -29,17 +38,23 @@ if echo "$response" | jq -e '.[] | select(.Name == "'"$STACK_NAME"'")' > /dev/nu
 
   # Monta a URL para a exclusão
   DELETE_URL="$URL/$id"
-
+  
+  # verifica se o container existe. 
   if [ ! -z "$CONTAINER_ID" ]; then
     echo "pausando container"
-    curl -X POST "$URL_FOR_PAUSA/$CONTAINER_NAME/stop" -H "X-API-Key: $API_KEY"
+    curl -X POST "$MANIPULA_CONTAINER/$CONTAINER_NAME/stop" -H "X-API-Key: $API_KEY"
     sleep 5
+
     echo "deletando container"
-    curl -X DELETE "$URL_FOR_DELETE/$CONTAINER_NAME" -H "X-API-Key: $API_KEY"
-    echo "pull container"
-    docker-compose pull 
-    echo "deletando stack"
+    curl -X DELETE "$MANIPULA_CONTAINER/$CONTAINER_NAME" -H "X-API-Key: $API_KEY"
     sleep 5
+
+    # VALIDAR PROCESSO DE EXCLUSAO DA IMAGEM
+    echo "deletando imagem"
+    curl -s -X DELETE "$deleteimagem/$IMAGE_SHA" -H "X-API-Key: $API_KEY" --insecure
+    sleep 5
+
+    echo "deletando stack"
     curl -X DELETE "$DELETE_URL" \
     -H "X-API-Key: $API_KEY" \
     -F "type=2" \
@@ -48,13 +63,7 @@ if echo "$response" | jq -e '.[] | select(.Name == "'"$STACK_NAME"'")' > /dev/nu
     -F "endpointId=$ENDPOINT" \
     -F "Name=$STACK_NAME" --insecure
     echo "Stack deletada. ID: $id"
-    sleep 5
-    echo "Aguarde 2 segundos"
-    sleep 2
-    echo "CRIANDO A STACK $name"
-    sleep 5
-    echo "Aguardando 5 segundos..."
-    sleep 5
+
     echo "=========================================="
     echo "CRIANDO A STACK $name"
     echo "=========================================="
@@ -75,44 +84,29 @@ if echo "$response" | jq -e '.[] | select(.Name == "'"$STACK_NAME"'")' > /dev/nu
     # Imprimir o valor do Id
     echo "Nova Stack criada. Id: $id"
   else
-    echo "container nao encontrado"
-    echo "deletando stack"
-    curl -X DELETE "$DELETE_URL" \
-    -H "X-API-Key: $API_KEY" \
-    -F "type=2" \
-    -F "method=file" \
-    -F "file=@$FILE_PATH" \
-    -F "endpointId=$ENDPOINT" \
-    -F "Name=$STACK_NAME" --insecure
-    echo "Stack deletada. ID: $id"
-    echo "CRIANDO A NOVA STACK"
-
-    response=$(curl -s -X POST "$URL" \
-    -H "X-API-Key: $API_KEY" \
-    -F "type=2" \
-    -F "method=file" \
-    -F "file=@$FILE_PATH" \
-    -F "endpointId=$ENDPOINT" \
-    -F "Name=$STACK_NAME" --insecure)
-
-    # Imprimir a resposta da requisição 
-    echo "Resposta da solicitação POST: $response"
-
-    # Extrair o valor do campo "Id" da nova stack usando jq
-    id=$(echo "$response" | jq -r '.Id')
-
-    # Imprimir o valor do Id
-    echo "Nova Stack criada. Id: $id"
+    echo "stack encontrada, mas container não encontrado??"
+    exit 1
   fi
 
 else
   echo "=========================================="
   echo "NENHUMA STACK DA APLICAÇÃO ENCONTRADA."
+
+  echo "pausando container"
+    curl -X POST "$MANIPULA_CONTAINER/$CONTAINER_NAME/stop" -H "X-API-Key: $API_KEY"
+    sleep 5
+
+  echo "deletando container"
+  curl -X DELETE "$MANIPULA_CONTAINER/$CONTAINER_NAME" -H "X-API-Key: $API_KEY"
+  sleep 5
+
+  # VALIDAR PROCESSO DE EXCLUSAO DA IMAGEM
+  echo "deletando imagem"
+    curl -s -X DELETE "$deleteimagem/$IMAGE_SHA" -H "X-API-Key: $API_KEY" --insecure
+    sleep 5
+
   echo "CRIANDO A NOVA STACK"
   echo "=========================================="
-  echo "realizando o pull"
-  docker pull hugollemos/demo:latest
-  sleep 5
   response=$(curl -s -X POST "$URL" \
   -H "X-API-Key: $API_KEY" \
   -F "type=2" \
